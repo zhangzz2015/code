943901dc2f401283f02353cb7988a00117b51319
kai <zk.zhengkai@outlook.com>
2020-06-29 18:59:29 -0400

    06/28/2020

--- algorithms/java/fun_random_generator/RandomGenerator.java
+++ algorithms/java/fun_random_generator/RandomGenerator.java
@@ -1,3 +1,5 @@
+import  java.util.Random;
+
 public class RandomGenerator {
     private int[] ranges;
 	private final String[] symbols;
@@ -9,14 +11,14 @@ public class RandomGenerator {
 	    this.random = new Random();
 	    ranges = new int[weights.length];
 	    ranges[0] = weights[0];
-	    for (int i = 0; i < weights.length; i++) {
+	    for (int i = 1; i < weights.length; i++) {
 	       ranges[i] = ranges[i - 1] + weights[i];
         }
     }
 
     public String getRandom() {
 	    int randomWeight = random.nextInt(ranges[ranges.length - 1]);
-	    return symbols[smallestLarger(randomWeights)];
+	    return symbols[smallestLarger(randomWeight)];
     }
 
     private int smallestLarger(int randomWeight) {
@@ -31,5 +33,27 @@ public class RandomGenerator {
             }
         }
         return left;
-    }
+	}
+	
+	public static void main(String[] args) {
+		String[] symbols = {"A", "B", "C", "D"};
+		int[] weights = {40, 30, 20, 10};
+		RandomGenerator solution = new RandomGenerator(symbols, weights);
+		double a = 0;
+		double b = 0;
+		double c = 0;
+		double d = 0;
+		int total = 1000;
+		for (int i = 0; i < total; i++) {
+			String tmp = solution.getRandom();
+			if (tmp == "A") a++;
+			else if (tmp == "B") b++;
+			else if (tmp == "C") c++;
+			else d++;
+		}
+		System.out.println("percentage of A:" + a / total);
+		System.out.println("percentage of B:" + b / total);
+		System.out.println("percentage of C:" + c / total);
+		System.out.println("percentage of D:" + d / total);
+	}
 }
