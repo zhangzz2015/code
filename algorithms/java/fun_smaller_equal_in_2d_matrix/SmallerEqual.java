fec005490310bc15dd1ca821ebd5e9727834f86c
kai <zk.zhengkai@outlook.com>
2020-07-05 17:57:21 -0400

    07/05/2020

---
+++ algorithms/java/fun_smaller_equal_in_2d_matrix/SmallerEqual.java
@@ -0,0 +1,18 @@
+class SmallerEqual {
+    public boolean find(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) return 0;
+        int i = 0;
+        int j = matrix[0].length - 1;
+        int count = 0;
+        while (i < matrix.length && j >= 0) { // i, j must be in the matrix
+            if (matrix[i][j] > target) j--; // still on the current row finding the rightmost j
+            else { // on the current row first time meet the rightmost j, matrix[i][j] <= target
+                i++; // go to the next row
+                count += j + 1;
+            }
+        }
+        // !!! important implementation detail: terminate condition
+        // i == matrix.length || j == -1
+        return count;
+    }
+}
