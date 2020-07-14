0cd05d405670178a7eac981f41fbc698b0362283
kai <zk.zhengkai@outlook.com>
2020-07-13 22:15:05 -0400

    07/13/2020

--- algorithms/java/274_h_index/HIndex.java
+++ algorithms/java/274_h_index/HIndex.java
@@ -1,6 +1,7 @@
 class HIndex {
     public int hIndex(int[] citations) {
         if (citations.length == 0) return 0;
+        // need one more bucket for the numbers that are larger than length
         int[] counts = new int[citations.length + 1];
         for (int i = 0; i < citations.length; i++) {
             if (citations[i] > citations.length) {
