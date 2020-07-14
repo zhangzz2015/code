166cf0e9a65db825c74becf653671ad6e2569b7d
kai <zk.zhengkai@outlook.com>
2020-07-13 22:11:49 -0400

    07/13/2020

---
+++ algorithms/java/274_h_index/HIndex.java
@@ -0,0 +1,21 @@
+class HIndex {
+    public int hIndex(int[] citations) {
+        if (citations.length == 0) return 0;
+        int[] counts = new int[citations.length + 1];
+        for (int i = 0; i < citations.length; i++) {
+            if (citations[i] > citations.length) {
+                counts[citations.length] += 1;
+            } else {
+                counts[citations[i]] += Math.min(1, citations[i]);
+            }
+        }
+        int citationCount = 0;
+        for (int i = counts.length - 1; i >= 0; i--) {
+            citationCount += counts[i];
+            if (i <= citationCount) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
