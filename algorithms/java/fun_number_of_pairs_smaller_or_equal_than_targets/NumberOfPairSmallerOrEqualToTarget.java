5fcee275b83420795ddcf55584f8429f72043f4a
kai <zk.zhengkai@outlook.com>
2020-08-10 19:31:26 -0400

    08/10/2020

---
+++ algorithms/java/fun_number_of_pairs_smaller_or_equal_than_targets/NumberOfPairSmallerOrEqualToTarget.java
@@ -0,0 +1,19 @@
+public class NumberOfPairSmallerOrEqualToTarget {
+    public int  numberOfPairSmallerOrEqualToTarget(int arr, int target) {
+        if (arr == null) return 0;
+        int left = 0;
+        int right = arr.length - 1;
+        int res = 0;
+        while (left < right) {
+            while (arr[left] + arr[right] <= target) {
+                left++;
+            }
+            res += left;
+            right--;
+        }
+        // sum of any pair from index [0, index] <= target.
+        // c(n, 2) = n *  (n - 1) / 2
+        res += (right + 1) * right / 2;
+        return res;
+    }
+}
