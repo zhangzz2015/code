6d3fed05404904f5e0a9e302113688ef60c68fea
kai <zk.zhengkai@outlook.com>
2020-08-28 16:52:33 -0400

    07/09/2020

--- algorithms/java/116_populating_next_right_pointers/PopulateNext.java
+++ algorithms/java/116_populating_next_right_pointers/PopulateNext.java
@@ -6,7 +6,26 @@ public class PopulateNext {
         public Node next;
     }
 
-    public Node connect(Node root) {
+    // 1 basic bfs 
+    public void populate1(Node root) {
+        if (root == null) return;
+        Queue<Node> queue = new ArrayDeque<>();
+        queue.offer(root);
+        while (!queue.isEmpty()){
+            int size = queue.size();
+            Node prev = null;
+            for (int i = 0; i < size; i++) {
+                Node cur = queue.poll();
+                if (prev != null) prev.next = cur;
+                prev = cur;
+                if (cur.left != null) queue.offer(cur.left);
+                if (cur.right != null) queue.offer(cur.right);
+            }
+        }
+    } 
+
+    // 2 space optimize
+    public void populate2(Node root) {
         if (root == null) return root;
         Node nextLevelHead = null;
         Node curNode = root;
@@ -37,6 +56,22 @@ public class PopulateNext {
                 prevNode = null;
             }
         }
-        return root;
+    }
+
+    // DFS
+    public void populate3(Node root) {
+        List<Node> tails = new ArrayList<>();
+        dfs(tails, root, 0);
+    }
+    private void dfs(List<Node> tials, Node root, int level) {
+        if (root == null) return;
+        if (tails.size() == level) {
+            tails.add(root);
+        } else {
+            tails.get(level).next = root;
+            tails.set(level, root);
+        }
+        dfs(tails, root.left, level + 1);
+        dfs(tails, root.right, level + 1);
     }
 }
