b5fc5d56a6bec78b307aeb8d4039cadaa82a71d7
kai <zk.zhengkai@outlook.com>
2020-08-31 01:05:12 -0400

    08/31/2020

---
+++ algorithms/java/fun_any_to_any_largest_path_sum/PathSum.java
@@ -0,0 +1,15 @@
+public class PathSum {
+    
+    int globalMax = Integer.MIN_VALUE;
+    public int largest(TreeNode root) {
+        // base case
+        if (root == null) return 0;
+        // recursion
+        int left = Math.max(0, largest(root.left));
+        int right = Math.max(0, largest(root.right));
+        // update global max
+        int largest = left + right + root.val;
+        globalMax = Math.max(largest, globalMax);
+        return Math.max(left, right) + root.val;
+    }
+}
