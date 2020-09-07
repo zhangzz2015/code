048e373dedfc5c578bd6c83bd845d915259ccf0c
kai <zk.zhengkai@outlook.com>
2020-09-07 02:49:37 -0400

    09/07/2020

---
+++ algorithms/java/fun_collect_leaf_and_remove/CollectLeaves.java
@@ -0,0 +1,18 @@
+public class CollectLeaves.java {
+	public List<List<Integer>> collect(TreeNode root) {
+		List<List<Integer>> res = new ArrayList<>();
+		helper(root, res);
+		return res;
+	}
+	private int helper(TreeNode root, List<List<Integer>> res) {
+		if (root == null) return 0;
+		int left = helper(root.left, res);
+		int right = helper(root.right, res);
+		int height = Math.max(left, right) + 1;
+		if (res.size() < height) {
+			res.add(new ArrayList<>());
+		}
+		res.get(height - 1).add(root.val);
+		return height;
+	}
+}
