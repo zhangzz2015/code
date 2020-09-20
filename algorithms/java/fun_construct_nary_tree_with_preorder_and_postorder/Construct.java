54d610423de7f7a0558a809ac11594bb38136d9f
kai <zk.zhengkai@outlook.com>
2020-09-20 01:05:24 -0400

    09/20/2020

---
+++ algorithms/java/fun_construct_nary_tree_with_preorder_and_postorder/Construct.java
@@ -0,0 +1,15 @@
+class Construct {
+    // 2. both in recursion
+    int preIndex = 0;
+    int postIndex = 0;
+    public TreeNode construct(String[] pre, String[] post) {
+        TreeNode root = new TreeNode(pre[preIndex]);
+        preIndex++;
+        // when the next element in post order is root itself, we know the current tree is complete
+        while (!post[posIndex].equals(root.val)) {
+            root.children.add(construct(pre, post));
+        }
+        postIndex++;
+        return root;
+    }
+}
