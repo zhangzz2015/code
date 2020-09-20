495b2f6c60c4fe8b335eb2e5592e3ade7f825468
kai <zk.zhengkai@outlook.com>
2020-09-20 17:23:02 -0400

    09/20/2020

---
+++ algorithms/java/428_serialize_and_deserialize_nary_tree/SerDeser.java
@@ -0,0 +1,42 @@
+class SerDeser {
+    
+    public String serialize(Node root) {
+        if (root == null) {
+            return "";
+        }
+        StringBuilder sb = new StringBuilder();
+        dfs(root, sb);
+        return sb.toString();
+    }
+    private void dfs(Node root, StringBuilder sb) {
+        sb.append("[");
+        sb.append(root.val);
+        for (Node child : root.children) {
+            dfs(child, sb);
+        }
+        sb.append("]");
+    }
+
+    
+    public Node deserialize(String data) {
+        if (data == null || data.length() == 0) {
+            return null;
+        }
+        int[] index = new int[1];
+        return construct(data, index);
+    }
+    private Node construct(String data, int[] index) {
+        index[0]++;
+        int nextInt = 0;
+        while (data.charAt(index[0]) != '[' && data.charAt(index[0]) != ']') {
+            nextInt = nextInt * 10 + data.charAt(index[0]) - '0';
+            index[0]++;
+        }
+        Node root = new Node(nextInt, new ArrayList<>());
+        while (data.charAt(index[0]) != ']') {
+            root.children.add(construct(data,index));
+        }
+        index[0]++;
+        return root;
+    }
+}
