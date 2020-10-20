2980ad3d6804e7546d519b2765371908b2511c33
zzzKai <zk.zhengkai@outlook.com>
2020-10-20 18:43:55 -0400

    10/20/2020

---
+++ algorithms/java/fun_hours_of_burn_down_the_whole_tree/HoursOfBurnDown.java
@@ -0,0 +1,29 @@
+class HoursOfBurnDown {
+	public int calHours(TreeNode root, TreeNode start) {
+		if (start == null) {
+			// not on fire
+			return Integer.MAX_VALUE;
+		}
+		int[] max = new int[]{0};
+		calHours(root, start, max);
+		return max[0];
+	}
+	private int calHours(TreeNode root, TreeNode start, int[] max) {
+		if (root == null) return 0;
+		int left = calHours(root.left, start, max);
+		int right = calHours(root.right, start, max);
+		if (root == start) {
+			max[0] = Math.max(max[0], Math.max(left, right) + 1);
+			return -1;
+		}
+		if (left >= 0 && right >= 0) {
+			return Math.max(left, right) + 1;
+		} else if (left < 0) {
+			max[0] = Math.max(max[0], right - left);
+			return left - 1;
+		} else if (right < 0) {
+			max[0] = Math.max(max[0], left - right);
+			return right - 1;
+		}
+	}
+}
