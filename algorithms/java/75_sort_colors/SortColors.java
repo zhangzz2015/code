a559a8a273756e60758e66bb21002c5b2942a74e
zzzKai <zk.zhengkai@outlook.com>
2020-10-28 21:37:11 -0400

    10/28/2020

---
+++ algorithms/java/75_sort_colors/SortColors.java
@@ -0,0 +1,23 @@
+class SortColors {
+	public void sortColors(int[] nums) {
+		if (nums == null || nums.length == 0) return;
+		int i = 0;
+		int j = 0;
+		int k = nums.length - 1;
+		while (j <= k) {
+			if (nums[j] == 0) {
+				swap(nums, i++, j++);
+			} else if (nums[j] == 1) {
+				j++;
+			} else {
+				swap(nums, j, k--);
+			}
+		}
+	}
+
+	private void swap(int[] nums, int i, int j) {
+		int tmp = nums[i];
+		nums[i] = nums[j];
+		nums[j] = tmp;
+	}
+}
