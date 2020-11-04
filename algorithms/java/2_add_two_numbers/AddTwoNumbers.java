f431f72e03a9b7f3e0442a0e1160efd3507d2c49
zzzKai <zk.zhengkai@outlook.com>
2020-11-04 16:32:41 -0500

    11/04/2020

---
+++ algorithms/java/2_add_two_numbers/AddTwoNumbers.java
@@ -0,0 +1,31 @@
+class AddTwoNumbers {
+	public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+		if (l1 == null) {
+			return l2;
+		}
+		if (l2 == null) {
+			return l1;
+		}
+		ListNode dummy = new ListNode(0);
+		ListNode cur = dummy;
+		int carry = 0;
+		while (l1 != null || l2 == null) {
+			int val1 = l1 == null ? 0 : l1.val;
+			int val2 = l2 == null ? 0 : l2.val;
+			int sum = val1 + val2 + carry;
+			cur.next = new ListNode(sum % 10);
+			carry = sum / 10;
+			if (l1 != null) {
+				l1 = l1.next;
+			}
+			if (l2 != null) {
+				l2 = l2.next;
+			}
+			cur = cur.next;
+		}
+		if (carry != 0) {
+			cur.next = new ListNode(carry);
+		}
+		return dummy.next;
+	}
+}
