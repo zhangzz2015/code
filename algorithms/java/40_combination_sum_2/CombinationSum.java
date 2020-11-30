50a1ab4bfd0e100ad165ca7f760757437a75fb56
zzzKai <zk.zhengkai@outlook.com>
2020-11-30 17:07:08 -0500

    11/30/2020

---
+++ algorithms/java/40_combination_sum_2/CombinationSum.java
@@ -0,0 +1,75 @@
+class CombinationSum {
+	// 1. 
+	public List<List<Integer>> combinationSum1(int[] candidates, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(candidates);
+        comb1(candidates, target, res, new ArrayList<>(), 0);
+        return res;
+    }
+    private void dfs(int[] candidates, int target, List<List<Integer>> res, List<Integer> cur, int index) {
+        if (target == 0) {
+            res.add(new ArrayList<>(cur));
+            return;
+        }
+        if (target < 0 || index == candidates.length) {
+            return;
+        }
+        cur.add(candidates[index]);
+        comb1(candidates, target - candidates[index], res, cur, index + 1);
+        cur.remove(cur.size() - 1);
+        while (index < candidates.length - 1 && candidates[index] == candidates[index + 1]) {
+            index++;
+        }
+        comb1(candidates, target, res, cur, index + 1);
+    }
+
+	// 2.
+	public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(candidates);
+        comb2(candidates, target, res, new ArrayList<>(), 0);
+        return res;
+    }
+    private void dfs(int[] candidates, int target, List<List<Integer>> res, List<Integer> cur, int index) {
+        if (target == 0) {
+            res.add(new ArrayList<>(cur));
+            return;
+        }
+        if (target < 0) {
+            return;
+        }
+        for (int i = index; i < candidates.length; i++) {
+            cur.add(candidates[i]);
+            comb2(candidates, target - candidates[i], res, cur, i + 1);
+            cur.remove(cur.size() - 1);
+            while (i < candidates.length - 1 && candidates[i] == candidates[i + 1]) {
+                i++;
+            }
+        }
+    }
+
+	// 3.
+	public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(candidates);
+        comb3(candidates, target, res, new ArrayList<>(), 0);
+        return res;
+    }
+    private void comb3(int[] candidates, int target, List<List<Integer>> res, List<Integer> cur, int index) {
+        if (target == 0) {
+            res.add(new ArrayList<>(cur));
+            return;
+        }
+        if (target < 0) {
+            return;
+        }
+        Set<Integer> set = new HashSet<>();
+        for (int i = index; i < candidates.length; i++) {
+            if (set.add(candidates[i])) {
+                cur.add(candidates[i]);
+                comb3(candidates, target - candidates[i], res, cur, i + 1);
+                cur.remove(cur.size() - 1);
+            }
+        }
+    }
+}
---
+++ algorithms/java/40_combination_sum_2/README.md
@@ -0,0 +1,32 @@
+In this problem we have duplicates in the candidates, and we are finding the value combination instead of the index combination.
+## backtracking
+we choose to pick or not pick the element at each index. To avoid duplicate situation when we have duplicate element we will have [1, 2] and [1, 2] which use the different 1s as the answer. That means if we don't choose to pick an element we will not choose the same value again. To acheive this, we want to group the elements together and when we do not pick this element we want to skip all the same value elements. To group them together we can sort the candidates.<br>
+
+			sorted candidates [1, 1, 2, 2, 5] target = 5
+
+			                     ""
+						       /    \
+							  1     ""
+							 / \       |skip
+							11   1     |
+						   / \  / \   /  \
+						112  11 12 1  2  ""
+						...
+
+time: O(2^n)<br>
+space: O(n)
+
+## backtracking
+we choose all the possible candidate and add to the sum for each level. To avoid the result to appear [1, 2], [2, 1], we can perform an order trick, for each level we want to choose from the candidates from that candidate and candidates after that candidate. We will not rechoose the candidates before the current candidate. Thus, we need to sort the array first to avoid the same element appears before and after the current candidate like the case of [1, 2, 1]. To avoid duplicates result from the duplicate elements we want to only add one kind of the element in the same level. We can use a hashset to deduplicate or we can use sort and skip the same group trick like the first method.<br>
+
+			sorted candidates [1, 1, 2, 2, 5] target = 5
+
+			                          ""
+					           /         \    \ 
+							  1           2    5
+							 / \   \     / \   / \
+			                11  12 15   22 25 52 55
+							....
+
+time: O(nlogn + !n)<br>
+space: O(n)
