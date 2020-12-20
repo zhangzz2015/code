58aabe5be4d0c0c12104952a5237ed04e9a3621f
zzzKai <zk.zhengkai@outlook.com>
2020-12-20 00:55:21 -0500

    12/20/2020

--- data_structures/trie/Trie.java
+++ data_structures/trie/Trie.java
@@ -69,3 +69,66 @@ class Trie {
  * boolean param_2 = obj.search(word);
  * boolean param_3 = obj.startsWith(prefix);
  */
+
+
+
+
+
+/**
+* This class is a class of trie storing a set of the string
+* which is the prefix using the prefix starting from the root
+* ending at the current node before.
+*/
+class Trie {
+	TrieNode root;
+
+	public Trie() {
+		this.root = new TrieNode();
+	}
+
+	public void insert(String word) {
+		if (word == null || word.length() == 0) {
+			return;
+		}
+		TrieNode cur = root;
+		cur.startsWith.add(word);
+		for(char c : word.toCharArray()) {
+			TrieNode next = cur.children.get(c);
+			if (next == null) {
+				next = new TrieNode();
+				cur.children.put(c, next);
+			}
+			cur = next;
+			cur.startsWith.add(word);
+		}
+		cur.isWord = true;
+	}
+
+	/* return a list of the strings starting with the prefix */
+	public List<String> startsWith(String prefix) {
+		List<String> res = new ArrayList<>();
+		TrieNode cur = root;
+		for (char c : prefix.toCharArray()) {
+			if (cur.children.get(c - 'a') == null) {
+				return res;
+			}
+			cur = cur.get(c - 'a');
+		}
+		res.addAll(cur.startsWith);
+		return res;
+	}
+
+	class TrieNode {
+		boolean isWord;
+		Set<String> startsWith;
+		Map<Character, TrieNode> children;
+		TrieNode() {
+			this.isWord;
+			this.startsWith = new HashSet<>();
+			this.children = new HashMap<>();
+		}
+	}
+}
+
+
+
