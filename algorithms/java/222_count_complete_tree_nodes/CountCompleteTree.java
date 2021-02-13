b53d459ca7c2ac8854cc93331c8bd689fd609ed0
zzzKai <zk.zhengkai@outlook.com>
2021-02-12 23:37:02 -0500

    02/12/2021

--- algorithms/java/222_count_complete_tree_nodes/CountCompleteTree.java
+++ algorithms/java/222_count_complete_tree_nodes/CountCompleteTree.java
@@ -20,4 +20,41 @@ public class CountCompleteTree {
         }
         return countCompleteTree(root.left) + countCompleteTree(root.right) + 1;
     }
+
+	// recursion
+	public int countNodes1(TreeNode root) {
+		if (root == null) return 0;
+		int leftHeight = getHeight(root.left);
+		int rightHeight = getHeight(root.right);
+		if (leftHeight == rightHeight) {
+			return (1 << leftHeight) + countNodes(root.right);
+		}
+		return (1 << rightHeight) + countNodes(root.left);
+	}
+
+	// iterative
+	public int countNodes2(TreeNode root) {
+		int count = 0;
+		while (root != null) {
+			int leftHeight = getHeight(root.left);
+			int rightHeight = getHeight(root.right);
+			if (leftHeight == rightHeight) {
+				count += 1 << leftHeight;
+				root = root.right;
+			} else {
+				count += 1 << rightHeight;
+				root = root.left;
+			}
+		}
+		return count;
+	}
+
+	private int getHeight(TreeNode root) {
+		int height = 0;
+		while (root != null) {
+			height++;
+			root = root.left;
+		}
+		return height;
+	}
 }
