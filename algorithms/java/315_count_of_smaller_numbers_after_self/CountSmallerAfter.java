e64c7ddc325ef962bc3a58569d4a939830d80d80
zzzKai <zk.zhengkai@outlook.com>
2021-02-24 19:27:00 -0500

    02/24/2021

--- algorithms/java/315_count_of_smaller_numbers_after_self/CountSmallerAfter.java
+++ algorithms/java/315_count_of_smaller_numbers_after_self/CountSmallerAfter.java
@@ -1,40 +1,44 @@
 public class CountSmallerAfter {
-    public List<Integer> countSmaller(int[] nums) {
-        int[] index = new int[nums.length];
-        for (int i = 0; i < index.length; i++) index[i] = i;
-        int[] count = new int[nums.length];
-        mergeSort(nums, index, count, 0, nums.length - 1);
-        List<Integer> res = new ArrayList<>();
-        for (int i : count) res.add(i);
-        return res;
-    }
-
-    private void mergeSort(int[] nums, int[] index, int[] count, int left, int right) {
-        if (left >= right) return;
-        int mid = left + (right - left) / 2;
-        mergeSort(nums, index, count, left, mid);
-        mergeSort(nums, index, count, mid + 1, right);
-        merge(nums, index, count, left, mid, right);
-    }
-
-    private void merge(int[] nums, int[] index, int[] count, int left, int mid, int right) {
-        int[] tmp = new int[right - left + 1];
-        int leftIndex = left;
-        int rightIndex = mid + 1;
-        int tmpIndex = 0;
-        while (leftIndex <= mid) {
-            if (rightIndex > right || nums[index[leftIndex]] <= nums[index[rightIndex]]) {
-                count[index[leftIndex]] += rightIndex - mid - 1;
-                tmp[tmpIndex++] = index[leftIndex++];
-            } else {
-                tmp[tmpIndex++] = index[rightIndex++];
-            }
-        }
-        while (rightIndex <= right) {
-            tmp[tmpIndex++] = index[rightIndex++];
-        }
-        for (int i = 0, z = left; z <= right; i++, z++) {
-            index[z] = tmp[i];
-        }
-    }
+	public List<Integer> countSmaller(int[] nums) {
+		if (nums == null || nums.length == 0) {
+			return new ArrayList<>();
+		}
+		int[] index = new int[nums.length];
+		for (int i = 0; i < index.length; i++) {
+			index[i] = i;
+		}
+		int[] count = new int[nums.length];
+		int[] helper = new int[nums.length];
+		mergeSort(nums, index, count, helper, 0, nums.length - 1);
+		List<Integer> res = new ArrayList<>();
+		for (int c : count) {
+			res.add(c);
+		}
+		return res;
+	}
+	private void mergeSort(int[] nums, int[] index, int[] count, int[] helper, int left, int right) {
+		if (left >= right) {
+			return;
+		}
+		int mid = left + (right - left) / 2;
+		mergeSort(nums, index, count, helper, left, mid);
+		mergeSort(nums, index, count, helper, mid + 1, right);
+		merge(nums, index, count, helper, left, mid, right);
+	}
+	private void merge(int[] nums, int[] index, int[] count, int[] helper, int left, int mid, int right) {
+		for (int i = left; i <= right; i++) {
+			helper[i] = index[i];
+		}
+		int l = left;
+		int r = mid + 1;
+		int cur = left;
+		while (l <= mid) {
+			if (r > right || nums[helper[l]] <= nums[helper[r]]) {
+				count[helper[l]] += r - mid - 1;
+				index[cur++] = helper[l++];
+			} else {
+				index[cur++] = helper[r++];
+			}
+		}
+	}
 }
