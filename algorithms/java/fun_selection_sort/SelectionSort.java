bec23d6f1c1bfbf5c8f8b34b86d3222b08d3c976
Kai <38972364+zzzKai@users.noreply.github.com>
2021-05-16 23:13:48 -0400

    05/16/2021

--- algorithms/java/fun_selection_sort/SelectionSort.java
+++ algorithms/java/fun_selection_sort/SelectionSort.java
@@ -1,49 +1,46 @@
 import java.util.Arrays;
 
 public class SelectionSort {
-    public int[] selectionSort(int[] array) {
+    public void selectionSort(int[] array) {
         // sanity check
         if (array == null || array.length <= 1) {
             return array;
         }
-        for (int i = 0; i < array.length - 1; i++) {
-            int min = i;
-            for (int j = i + 1; j < array.length; j++) {
-                if (array[j] < array[min]) {
-                    min = j;
-                }
-            }
-            swap(array, i, min);
-        }
-        return array;
+        int len = array.length;
+		for (int i = 1; i < len; i++) {
+			int cur = array[i];
+			int j = i - 1
+			for (; j >= 0; j--) {
+				if (array[j] > cur) {
+					array[j + 1] = array[j];
+				} else {
+					break;
+				}
+			}
+			array[j + 1] = cur;
+		}
     }
-
-    private void swap(int[] array, int left, int right) {
-        int tmp = array[left];
-        array[left] = array[right];
-        array[right] = tmp;
-    }
-
+	
     public static void main(String[] args) {
         SelectionSort selectionSort = new SelectionSort();
         int[] array = null;
-        array = selectionSort.selectionSort(array);
+        selectionSort.selectionSort(array);
         System.out.println(Arrays.toString(array));
         
         array = new int[0];
-        array = selectionSort.selectionSort(array);
+        selectionSort.selectionSort(array);
         System.out.println(Arrays.toString(array));
         
         array = new int[] {1, 2, 3, 4};
-        array = selectionSort.selectionSort(array);
+        selectionSort.selectionSort(array);
         System.out.println(Arrays.toString(array));
 
         array = new int[] {4, 3, 2, 1};
-        array = selectionSort.selectionSort(array);
+        selectionSort.selectionSort(array);
         System.out.println(Arrays.toString(array));
 
         array = new int[] {2, 4, 1, 5, 3};
-        array = selectionSort.selectionSort(array);
+        selectionSort.selectionSort(array);
         System.out.println(Arrays.toString(array));
     }
 }
