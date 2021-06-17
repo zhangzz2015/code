64552e3e659046d4ff1f66158ea34b38fc789cac
zzzKai <zk.zhengkai@outlook.com>
2021-06-16 23:54:16 -0400

    06/16/2021

--- algorithms/cpp/57_insert_interval/Insert.cpp
+++ algorithms/cpp/57_insert_interval/Insert.cpp
@@ -1,4 +1,25 @@
 class Insert {
 public:
-	
+	vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        vector<vector<int>> res;
+        bool inserted = false;
+        for (auto cur : intervals) {
+            if (cur[1] < newInterval[0]) {
+                res.push_back(cur);
+            } else if (cur[0] > newInterval[1]) {
+                if (!inserted) {
+                    res.push_back(newInterval);
+                    inserted = true;
+                }
+                res.push_back(cur);
+            } else {
+                newInterval[0] = min(newInterval[0], cur[0]);
+                newInterval[1] = max(newInterval[1], cur[1]);
+            }
+        }
+        if (!inserted) {
+            res.push_back(newInterval);
+        }
+        return res;
+    }
 };
