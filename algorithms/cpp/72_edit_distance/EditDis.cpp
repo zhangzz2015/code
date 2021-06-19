6f48002351798eebb7451a61b31b84d201bffd05
zzzKai <zk.zhengkai@outlook.com>
2021-06-19 17:39:22 -0400

    06/19/2021

---
+++ algorithms/cpp/72_edit_distance/EditDis.cpp
@@ -0,0 +1,21 @@
+class EditDis {
+public:
+	int minDistance(string word1, string word2) {
+        int n = word1.size(), m = word2.size();
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1));
+        for (int i = 0; i <= n; ++i) {
+            for (int j = 0; j <= m; ++j) {
+                if (i == 0) {
+                    dp[0][j] = j;
+                } else if (j == 0) {
+                    dp[i][0] = i;
+                } else if (word1[i - 1] == word2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = min(dp[i - 1][j], min(dp[i][j - 1], dp[i - 1][j - 1])) + 1;
+                }
+            }
+        }
+        return dp[n][m];
+    }
+};
