3b8ac1ef77e4d2e95c8d336c329bf44cac21dedf
zzzKai <zk.zhengkai@outlook.com>
2021-06-20 16:03:14 -0400

    06/20/2021

---
+++ algorithms/cpp/74_search_2d_matrix/Search.cpp
@@ -0,0 +1,20 @@
+class Search {
+public:
+	bool searchMatrix(vector<vector<int>>& matrix, int target) {
+		int m = matrix.size(), n = matrix[0].size();
+		int left = 0, right = m * n - 1;
+		while (left <= right) {
+			int mid = left + (right - left) / 2;
+			int row = mid / n;
+			int col = mid % n;
+			if (matrix[row][col] == target) {
+				return true;
+			} else if (matrix[row][col] > target) {
+				right = mid - 1;
+			} else {
+				left = mid + 1;
+			}
+		}
+		return false;
+	}
+};
