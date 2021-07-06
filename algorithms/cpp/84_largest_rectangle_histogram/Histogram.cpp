492445c344c4eac63551bc7eb57c9ab4709bd18a
Kai <38972364+zzzKai@users.noreply.github.com>
2021-07-06 15:41:55 -0700

    07/06/2021

--- algorithms/cpp/84_largest_rectangle_histogram/Histogram.cpp
+++ algorithms/cpp/84_largest_rectangle_histogram/Histogram.cpp
@@ -2,16 +2,16 @@ class Histogram {
 public:
 	int largestRectangleArea(vector<int>& heihgts) {
 		int res = 0;
-		deque<int> stack;
+		stack<int> stack;
 		for (int right = 0; right <= heights.size(); ++right) {
 			int cur = right == heights.size() ? 0 : heights[right];
-			while (!stack.empty() && heights[stack.back()] >= cur) {
-				int height = heights[stack.back()];
-				stack.pop_back();
-				int left = stack.empty() ? -1 : stack.back();
+			while (!stack.empty() && heights[stack.top()] >= cur) {
+				int height = heights[stack.top()];
+				stack.pop();
+				int left = stack.empty() ? -1 : stack.top();
 				res = max(res, height * (right - left - 1));
 			}
-			stack.push_back(right);
+			stack.push(right);
 		}
 		return res;
 	}
